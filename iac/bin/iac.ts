7b43e34890ee6cf2d5c671fddeb51f4071ce5480
Jestin Philip <jestin.philip@icloud.com>
2025-11-30 14:13:21 +0000

    testcode ignore

--- iac/bin/iac.ts
+++ iac/bin/iac.ts
@@ -10,7 +10,10 @@ new PersonalWebsiteStack(app, 'PersonalWebsiteStack', {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: "eu-west-2" },
+  env: { 
+    account: process.env.CDK_DEFAULT_ACCOUNT, 
+    region: "eu-west-2" 
+  },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
--- iac/lib/PersonalWebsiteStack.ts
+++ iac/lib/PersonalWebsiteStack.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs';
 import * as lightsail from 'aws-cdk-lib/aws-lightsail';
 import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class PersonalWebsiteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -14,5 +15,13 @@ export class PersonalWebsiteStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'IacQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    // Create S3 bucket
+    /*
+    const bucket = new s3.Bucket(this, 'MyBucket', {
+      bucketName: 'my-cdk-example-bucket-jphilip-12345678910', // must be globally unique
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // delete bucket when stack is deleted (optional)
+      autoDeleteObjects: true,                 // delete objects too (optional)
+    });*/
   }
 }
